62e92e3534279a6aa1e36064cf89222440574363
MOD-999 <mohammadqatanani83@outlook.com>
2026-04-13 07:38:08 +0300

    Add loans app with BorrowRequest model and initial migration
    - Created loans app with models, views, and admin registration.
    - Implemented BorrowRequest model with fields for book, user, request date, and status.
    - Added initial migration for the loans app.

--- Library/catalog/models.py
+++ Library/catalog/models.py
@@ -2,3 +2,9 @@ from django.db import models
 
 # Create your models here.
 
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=150)
+    category = models.CharField(max_length=50)
+    is_available = models.BooleanField(default=True)
+
---
+++ Library/loans/models.py
@@ -0,0 +1,29 @@
+from django.conf import settings
+from django.db import models
+
+
+# Create your models here.
+
+class BorrowRequest(models.Model):
+    book = models.ForeignKey(
+        "catalog.Book",
+        on_delete=models.CASCADE,
+        related_name="borrow_requests"
+    )
+    
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="my_requests"
+    )
+
+    request_date = models.DateTimeField(auto_now_add=True)
+    
+    status =models.CharField(
+        max_length=20,
+        choices=[("P","Pending"),("A","Approved"),("R","Rejected")],
+        default="P"
+    )
+
+    def __str__(self):
+        return f"{self.user.username} requested {self.book.title}"

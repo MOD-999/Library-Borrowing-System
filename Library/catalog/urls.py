ce859d883572770e9bee5c859c84bce90df00ad3
MOD-999 <mohammadqatanani83@outlook.com>
2026-04-14 16:57:51 +0300

    feat(catalog): add DRF book endpoints and current user API

--- Library/catalog/urls.py
+++ Library/catalog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from . import views
+from .views import BookListCreateAPI, BookDetailAPI, CurrentUserView
 
-urlpatterns= [
-    path('',views.index,name = "index"),
+urlpatterns = [
+    path("books/", BookListCreateAPI.as_view(), name="book-list-create"),
+    path("books/<int:pk>/", BookDetailAPI.as_view(), name="book-detail"),
+    path("me/", CurrentUserView.as_view(), name="current-user"),
 ]
--- Library/catalog/views.py
+++ Library/catalog/views.py
@@ -1,7 +1,36 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Book
+from .serializer import BookSerializer
+from rest_framework import generics,permissions
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("<h1>test 1</h1>")
+class CurrentUserView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self,request):
+        return Response(
+            {
+                'id': request.user.id,
+                'username': request.user.username,
+                'is_staff': request.user.is_staff,
+            }
+        )
+
+class IsStaffOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request,view):
+        if request.method in permissions.SAFE_METHODS: 
+            return True
+        return request.user.is_staff
+
+
+class BookListCreateAPI(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsStaffOrReadOnly]
+
+class BookDetailAPI(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsStaffOrReadOnly]

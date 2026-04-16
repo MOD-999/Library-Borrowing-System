e615f2520e6cffefa8dd1ae1638609c43abb6f9c
MOD-999 <mohammadqatanani83@outlook.com>
2026-04-16 19:38:12 +0300

    Database: Update catalog models and add migrations

--- Library/catalog/models.py
+++ Library/catalog/models.py
@@ -5,6 +5,8 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=150)
-    category = models.CharField(max_length=50)
+    category = models.CharField(max_length=150)
+    image = models.ImageField(upload_to="book_images/", blank=True, null=True)
+    description = models.TextField(blank=True)
     is_available = models.BooleanField(default=True)
 
